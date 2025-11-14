d1037102d88e29bdbf8a3b62e2400a2724d65c42
champika ranaweera <cranaweera96@gmail.com>
2025-11-14 13:16:15 +0530

    take a product in dictionary type

--- Backend/product_dao.py
+++ Backend/product_dao.py
@@ -2,31 +2,47 @@ import mysql.connector
 
 query="select products.Product_id, products.name, products.uom_id, products.price_per_unit, uom.uom_name from products inner join uom on products.uom_id=uom.uom_id;"
 
-#insert data to db
-"""query1="INSERT INTO grocery_store.products (Product_id, name, uom_id, price_per_unit) VALUES (%s, %s, %s, %s)"
-values = (3, "banana", 2, 40)""" 
+def get_all_products():
+     
+    #insert data to db
+    """query1="INSERT INTO grocery_store.products (Product_id, name, uom_id, price_per_unit) VALUES (%s, %s, %s, %s)"
+    values = (3, "banana", 2, 40)""" 
 
-try:
-    connection = mysql.connector.connect(host='localhost', database='grocery_store', user='root',password='root')
-    cursor = connection.cursor()
-    """cursor.execute(query1,values)""" #insert data to db
-    cursor.execute(query)
-    """connection.commit()""" #for insert to db
+    try:
+        connection = mysql.connector.connect(host='localhost', database='grocery_store', user='root',password='root')
+        cursor = connection.cursor()
+        """cursor.execute(query1,values)""" #insert data to db
+        cursor.execute(query)
+        """connection.commit()""" #for insert to db
 
-    # Fetch results
-    """results = cursor.fetchall()
+        # Fetch results
+        """results = cursor.fetchall()
 
-    for row in results:
-        print(row)"""
-    
-    for (Product_id,name,uom_id,price_per_unit, uom_name) in cursor:
-        print(Product_id,name,uom_id,price_per_unit, uom_name)
+        for row in results:
+            print(row)"""
         
-except mysql.connector.Error as err:
-    print('Error :',err)
+        response = []
+        
+        for (Product_id,name,uom_id,price_per_unit, uom_name) in cursor:
+            response.append(
+                 {
+                      'Product_id' : Product_id,
+                      'name' : name,
+                      'uom_id' : uom_id,
+                      'price_per_unit' : price_per_unit,
+                      'uom_name' : uom_name
+                 }
+            )
+            
+    except mysql.connector.Error as err:
+        print('Error :',err)
 
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection closed.")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection closed.")
+            return response
+
+if __name__=='__main__':
+        print(get_all_products())
