a783ff7754c7dddc6733cb57e9308585d860a4ef
champika ranaweera <cranaweera96@gmail.com>
2025-11-14 13:08:46 +0530

    connect product table and uom table

--- Backend/product_dao.py
+++ Backend/product_dao.py
@@ -1,9 +1,10 @@
 import mysql.connector
 
-query="SELECT * FROM grocery_store.products"
+query="select products.Product_id, products.name, products.uom_id, products.price_per_unit, uom.uom_name from products inner join uom on products.uom_id=uom.uom_id;"
 
+#insert data to db
 """query1="INSERT INTO grocery_store.products (Product_id, name, uom_id, price_per_unit) VALUES (%s, %s, %s, %s)"
-values = (3, "banana", 2, 40)""" #insert data to db
+values = (3, "banana", 2, 40)""" 
 
 try:
     connection = mysql.connector.connect(host='localhost', database='grocery_store', user='root',password='root')
@@ -18,8 +19,8 @@ try:
     for row in results:
         print(row)"""
     
-    for (Product_id,name,uom_id,price_per_unit) in cursor:
-        print(Product_id,name,uom_id,price_per_unit)
+    for (Product_id,name,uom_id,price_per_unit, uom_name) in cursor:
+        print(Product_id,name,uom_id,price_per_unit, uom_name)
         
 except mysql.connector.Error as err:
     print('Error :',err)
