b41801b11d244a958a32bddd50e6c24c1c36a588
champika ranaweera <cranaweera96@gmail.com>
2025-11-16 19:05:13 +0530

    get product request

--- Backend/server.py
+++ Backend/server.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
+import product_dao
+from sql_connection import get_sql_connection
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def home():
-    return "Hello, this is a Flask server!"
+connection = get_sql_connection()
+
+@app.route("/getProduct", methods=['GET'])
+def get_products():
+    products = product_dao.get_all_products(connection)
+    response = jsonify(products)
+    response.headers.add("Access-Control-Allow-Origin",'*')
+    return response
+    
 
 if __name__ == "__main__":
     print("Flask server for Gs")
